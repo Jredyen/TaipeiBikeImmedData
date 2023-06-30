eb42b706db1c6abbf8bb52f2e3d31830689b533c
Jredyen <wii88051@gmail.com>
2023-06-30 16:05:09 +0800

    feat: Using AXIOS And Response 1.使用 Axios 連結 API 2.讀取前 100 個資料輸出至表格內

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 const jsonUrl = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json';
 
-const datas = document.querySelector('#datas')
+const table = document.querySelector('#datas')
+
+getData();
+async function getData(){
+    try{
+        const response = await axios.get(jsonUrl);
+        
+        response.data.slice(0,100).forEach(data => {
+
+            let tempHtml = '<tr>';
+            for (const key of Object.keys(data)) {
+                tempHtml += `<td>${data[key]}</td>`;
+            }
+            tempHtml += '</tr>';
+            return table.innerHTML += tempHtml
+            // return (
+            //     table.innerHTML += 
+            //     `<tr>
+            //     <td>${data.sno}</td>
+            //     <td>${data.sna}</td>
+            //     <td>${data.tot}</td>
+            //     <td>${data.sbi}</td>
+            //     <td>${data.sarea}</td>
+            //     <td>${data.mday}</td>
+            //     <td>${data.lat}</td>
+            //     <td>${data.lng}</td>
+            //     <td>${data.ar}</td>
+            //     <td>${data.sareaen}</td>
+            //     <td>${data.snaen}</td>
+            //     <td>${data.aren}</td>
+            //     <td>${data.bemp}</td>
+            //     <td>${data.act}</td>
+            //     <td>${data.srcUpdateTime}</td>
+            //     <td>${data.updateTime}</td>
+            //     <td>${data.infoTime}</td>
+            //     <td>${data.infoDate}</td>
+            //     </tr>`
+            // )
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
